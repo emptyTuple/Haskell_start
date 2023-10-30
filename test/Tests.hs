76b1414ddbcd5c6a57424e1070dfe3589cd83cc1
emptyTuple <kotorov@outlook.com>
2023-10-31 00:13:04 +0700

    cabal and tests games

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,7 +1,9 @@
 import              Test.Tasty (defaultMain, TestTree)
-import              Test.Tasty.HUnit (testCase, (@?=))              
+import              Test.Tasty.HUnit (testCase, (@?=))     
+
+import              Sqeq
 
 main :: IO ()
 main = 
-  defaultMain (testCase "x² + 2x + 1 = 0 -> [-1,-1]" (2+2 @?= 4))
+  defaultMain (testCase "x² + 2x + 1 = 0 -> [-1,-1]" (solveSquare 1 2 1 @?= [-1,-1]))
 
