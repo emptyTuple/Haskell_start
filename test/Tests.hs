daa88015a772b164b2b99d8b6369ef78dd3fd46c
emptyTuple <kotorov@outlook.com>
2023-10-31 00:20:04 +0700

    cabal and tests games

--- test/Tests.hs
+++ test/Tests.hs
@@ -5,5 +5,7 @@ import              Sqeq
 
 main :: IO ()
 main = 
-  defaultMain (testCase "x² + 2x + 1 = 0 -> [-1,-1]" (solveSquare 1 2 1 @?= [-1,-1]))
+  defaultMain $ 
+    testCase "x² + 2x + 1 = 0 -> [-1,-1]" $ 
+      solveSquare 1 2 1 @?= [-1,-1]
 
