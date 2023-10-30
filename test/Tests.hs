4b7ced16feef00b3a400f76da062b0302d445022
emptyTuple <kotorov@outlook.com>
2023-10-31 00:00:50 +0700

    cabal and tests games

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,4 +1,7 @@
-import Test.Tasty (defaultMain, TestTree)
+import              Test.Tasty (defaultMain, TestTree)
+import              Test.Tasty.HUnit (testCase, (@?=))              
 
 main :: IO ()
-main = defaultMain _
+main = 
+  defaultMain (testCase "x² + 2x + 1 = 0 -> [-1,-1]" (2+2 @?= 4))
+
