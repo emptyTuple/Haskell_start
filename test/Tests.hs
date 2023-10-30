87ee04efa933ddd66dd33025e08dcc2b7e8fcaf9
emptyTuple <kotorov@outlook.com>
2023-10-31 00:43:56 +0700

    cabal and tests games

--- test/Tests.hs
+++ test/Tests.hs
@@ -7,13 +7,13 @@ main :: IO ()
 main = 
   defaultMain $ 
     testGroup "test"
-      [ testCase "x² + 2x + 1 = 0 -> [-1,-1]" $ 
-          solveSquare 1 2 1 @?= [-1,-1]
-      , testCase "x² - 2x + 1 = 0 -> [1,1]" $
-          solveSquare 1 (-2) 1 @?= [1,1]
-      , testCase "x² - 2x - 3 = 0 -> [-1,3]" $
-          solveSquare 1 (-2) (-3) @?= [-1,3]
-      , testCase "-x² - 2x + 15 = 0 -> [3,-5]" $
-          solveSquare (-1) (-2) 15 @?= [3,-5]
+      [ testCase      "x² + 2x + 1 = 0 -> [-1,-1]" $ 
+          solveSquare    1 2 1 @?= [-1,-1]
+      , testCase      "x² - 2x + 1 = 0 -> [1,1]" $
+          solveSquare    1 (-2) 1 @?= [1,1]
+      , testCase      "x² - 2x - 3 = 0 -> [-1,3]" $
+          solveSquare    1 (-2) (-3) @?= [-1,3]
+      , testCase      "-x² - 2x + 15 = 0 -> [3,-5]" $
+          solveSquare    (-1) (-2) 15 @?= [3,-5]
       ]
 
