b936c209e71dabfd65e51d0d0c98081f8acc38fd
emptyTuple <kotorov@outlook.com>
2023-10-31 00:41:35 +0700

    cabal and tests games

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,4 +1,4 @@
-import              Test.Tasty (defaultMain, TestTree)
+import              Test.Tasty (defaultMain, TestTree, testGroup)
 import              Test.Tasty.HUnit (testCase, (@?=))     
 
 import              Sqeq
@@ -6,6 +6,14 @@ import              Sqeq
 main :: IO ()
 main = 
   defaultMain $ 
-    testCase "x² + 2x + 1 = 0 -> [-1,-1]" $ 
-      solveSquare 1 2 1 @?= [-1,-1]
+    testGroup "test"
+      [ testCase "x² + 2x + 1 = 0 -> [-1,-1]" $ 
+          solveSquare 1 2 1 @?= [-1,-1]
+      , testCase "x² - 2x + 1 = 0 -> [1,1]" $
+          solveSquare 1 (-2) 1 @?= [1,1]
+      , testCase "x² - 2x - 3 = 0 -> [-1,3]" $
+          solveSquare 1 (-2) (-3) @?= [-1,3]
+      , testCase "-x² - 2x + 15 = 0 -> [3,-5]" $
+          solveSquare (-1) (-2) 15 @?= [3,-5]
+      ]
 
